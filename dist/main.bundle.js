2eea53ff8e7dbd65fff37200f1a270de17f214f5
Zahra Hamdane <hamdane.zahra@gmail.com>
2023-06-19 23:01:38 +0200

    configuration webpack

---
+++ dist/main.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var test = "123";
+console.log(test);
+console.log($);
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
